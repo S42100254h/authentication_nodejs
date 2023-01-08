458182a23b2a44a5669920b79c7f2179c17aa7d7
yuki sawabe <sawabe0v0@gmail.com>
2023-01-08 22:51:22 +0900

    create method to register user

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require("express");
+const models = require("./models");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.status(200).send("Hello World!");
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    await models.user.create({
+      name: name,
+      email: email,
+      password: hashedPassword,
+    });
+    res.redirect("registerSuccess");
+  } catch (error) {
+    return res.status(400).json(error);
+  }
 });
 
 app.listen(port, () => console.log(`Application listening on port ${port}!`));
--- models/user.js
+++ models/user.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class user extends Model {
     /**
@@ -13,13 +11,16 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  user.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'user',
-  });
+  user.init(
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "user",
+    }
+  );
   return user;
 };
