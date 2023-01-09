cbc334ba09f506bafd038ab3b3d22152d7b8eb11
yuki sawabe <sawabe0v0@gmail.com>
2023-01-09 15:28:44 +0900

    create controller for user

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const session = require("express-session");
 
 const app = express();
 const port = 3000;
+
+app.use(
+  session({
+    secret: "secret",
+    saveUninitialized: true,
+    resave: true,
+    cookie: { maxAge: 60 * 60 * 1000 },
+  })
+);
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
