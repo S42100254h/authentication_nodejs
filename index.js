3704383ca20b42885fc2dcb5670f1a717fad6281
yuki sawabe <sawabe0v0@gmail.com>
2023-01-09 16:26:41 +0900

    create middleware for csrf

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const csrf = require("csurf");
 const session = require("express-session");
 
 const app = express();
 const port = 3000;
+
+const csrfProtection = csrf({ cookie: false });
 
 app.use(
   session({
@@ -19,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 
 const userRouter = require("./routes/user");
-app.use("/", userRouter);
+app.use("/", csrfProtection, userRouter);
 
 app.listen(port, () => console.log(`Application listening on port ${port}!`));
 
