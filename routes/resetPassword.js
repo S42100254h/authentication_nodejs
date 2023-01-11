506cce73239a62c2f71999d46b800b5a980bec2f
yuki sawabe <sawabe0v0@gmail.com>
2023-01-11 19:10:35 +0900

    create routes/index.js to put routes for displaying views together

--- routes/resetPassword.js
+++ routes/resetPassword.js
@@ -2,10 +2,6 @@ const express = require("express");
 const router = express.Router();
 const resetPasswordController = require("../controllers/resetPasswordController");
 
-router.get("/reset", (req, res) => {
-  res.render("resetPassword", { csrfToken: req.csrfToken() });
-});
-
 router.post("/reset", resetPasswordController.sendMail);
 
 module.exports = router;
